26b7910ff3b5513576f3caf87a4e4599edb3a8d0
HighOnMelatonin <tanruianh@outlook.com>
2024-02-04 10:17:08 +0800

    Changed reader to read db

--- reader.py
+++ reader.py
@@ -1,70 +1,60 @@
 ## Connect to database/log file
 
-import json
+from sqlite3 import connect
 import random
+import datetime
 
-raw = open('quotes.json', 'r+')
-quotes = json.load(raw)
-raw.close()
 
 class access:
     ## Defines methods to access quotes from the json file
+    quotes = connect('quotes_database.db')
+
     def getQuotesRan(category) -> str:
-        ## Returns a random quote from the given category
-        cats = list(quotes.keys())
-        ## Get a list of all the categories in the json
+        ## Method returns a random quote from the given category
+        try:
+            ## Try to get all quotes from selected category
+            catQuotes = quotes.execute("SELECT quote, author FROM ?", (category,))
+            ## Get a list of all the categories in the json
 
-        if not category in cats:
+            return random.choices(catQuotes)
+
+        except:
+            ## Raises exception when category does not exist
             return "Category does not exist"
 
-        else:
-            catQuotes = quotes[category]
-            return random.choices(catQuotes) 
-
-    def getQuotesLik(category) -> str:
+    def getQuotesLike(category) -> str:
         ## Returns a quote based on the number of likes
-        cats = list(quotes.keys())
+        try:
+            catQuotes = quotes.execute("SELECT quote FROM ?", (category,))
 
-        if not category in cats:
-            return "Category does not exist"
-
-        else:
-            catQuotes = quotes[category]
 
 
 class append:
     ## Defines methods to edit quotes in the json file
-    def addQuotes(category, quote) -> bool:
+    def addQuotes(category, quote, author):
         ## This method adds quotes to existing categories, returns True if successful, False if fails
-        cats = list(quotes.keys())
-        if not category in cats:
-            return False
-
-        else:
-            quotes[category].append(quote)
-
-            raw = open('quotes.json', 'r+')
-            json.dump(quotes, raw, indent = 2)
-            raw.close()
+        quotes = connect("quotes_database.db")
+        try:
+            date = datetime.datetime.today().strftime("%Y-%m-%d")
+            quotes.execute("INSERT INTO ? VALUES (?,?,?)",(category, quote, author, date,))
+            quotes.commit()
+            quotes.close()
             return True
 
-    def addCat(category) -> bool:
-        ## Method adds a new category to the json file, returns True if successful, False if the category already exists
-        cats = list(quotes.keys())
-        if not category in cats:
-            newCat = {category: []}
-            ## Add an empty array to the new category
-            quotes.update(newCat)
+        except:
+            quotes.close()
+            return "Category does not exist"
 
-            raw = open("quotes.json", 'r+')
-            json.dump(quotes, raw, indent=2)
-            raw.close()
-
+    def addCat(category):
+        ## This method adds a new category to the database
+        ## Adds a new category ONLY, this method does not add any quotes
+        try:
+            quotes = connect("quotes_database.db")
+            quotes.execute("CREATE TABLE ?(quote TEXT, author TEXT, date, TEXT)", (category,))
             return True
 
-        else:
+        except:
             return False
-
 
 test = access()
 test.getQuotes("Family")

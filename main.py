a8c969d1ec69c36a6ec935f42fce6632e7a4449f
Spenz7 <msrun3spencer@gmail.com>
2024-02-04 12:19:55 +0800

    uwuu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # app.py
 import reader
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -21,6 +21,10 @@ def random_quote(topic):
     # Replace this with your actual logic to select a random quote
     random_quote = reader.access.getQuotesRan(topic)
     return render_template('listofquotes.html', random_quote=random_quote, topic=topic)
+
+@app.route('/home')
+def home():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()

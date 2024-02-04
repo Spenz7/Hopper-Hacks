5cb50ff245e19d6bafbdacf184703901e7f55f59
Spenz7 <msrun3spencer@gmail.com>
2024-02-04 13:09:44 +0800

    YAYYYY

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-# Sample database code below
-# Replace this with your database connection logic
-quotes_database = {
-    'motivation': ['Motivation quote 1', 'Motivation quote 2', 'Motivation quote 3'],
-    # Add entries for other topics
-}
-
 @app.route('/')
 def index():
     return render_template('quotes.html')   
 
+oldquotes = []
 @app.route('/random/<topic>')
 def random_quote(topic):
     # Logic to get a random quote for the specified topic
     # Replace this with your actual logic to select a random quote
-    random_quote = reader.access.getQuotesRan(topic)
-    return render_template('listofquotes.html', random_quote=random_quote, topic=topic)
+    if len(oldquotes) == reader.access.getLength(topic):
+        newquote = 'There are no more quotes'
+        return render_template('listofquotes.html', random_quote=newquote, topic=topic)
+
+    newquote = reader.access.getQuotesRan(topic)
+    while newquote[0] in oldquotes: #2nd time onwards
+        newquote = reader.access.getQuotesRan(topic)
+    oldquotes.append(newquote[0])
+    return render_template('listofquotes.html', random_quote=newquote, topic=topic)
 
 @app.route('/home')
 def home():
